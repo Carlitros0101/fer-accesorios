1079285744ed53091ceee51feb87ca51f7907d7a
CarlitrosParra <carlosandresparra96@gmail.com>
2026-09-06 23:47:17 -0300

    Crear estructura editable del catálogo

---
+++ catalogo.js
@@ -0,0 +1,38 @@
+window.FER_CONFIG={
+  whatsappNumber:'56964133598',
+  whatsappCatalog:'https://wa.me/c/56964133598',
+  instagram:'https://www.instagram.com/fer_accesorioss_/'
+};
+
+window.FER_PRODUCTS=[
+  {
+    code:'FER-001',
+    name:'Diseño 01',
+    image:'assets/productos/WhatsApp%20Image%202026-09-06%20at%2011.00.24%20PM.jpeg',
+    category:'Accesorios',
+    price:null,
+    available:null,
+    isNew:true,
+    order:1
+  },
+  {
+    code:'FER-002',
+    name:'Diseño 02',
+    image:'assets/productos/WhatsApp%20Image%202026-09-06%20at%2011.00.36%20PM.jpeg',
+    category:'Accesorios',
+    price:null,
+    available:null,
+    isNew:true,
+    order:2
+  },
+  {
+    code:'FER-003',
+    name:'Diseño 03',
+    image:'assets/productos/WhatsApp%20Image%202026-09-06%20at%2011.00.50%20PM.jpeg',
+    category:'Accesorios',
+    price:null,
+    available:null,
+    isNew:true,
+    order:3
+  }
+];
